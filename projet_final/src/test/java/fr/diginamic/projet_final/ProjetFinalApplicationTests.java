8be8b18377fbaa7a3435e2f95a04fe5d67f7073b
Garcia <jimmygarcian@outlook.fr>
2022-01-25 17:24:43 +0100

    Création du Projet

---
+++ projet_final/src/test/java/fr/diginamic/projet_final/ProjetFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.diginamic.projet_final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
