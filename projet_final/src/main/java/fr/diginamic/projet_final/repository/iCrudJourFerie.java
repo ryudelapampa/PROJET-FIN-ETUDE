2a1eedd1489c1bde2662b0426a52d5e72319602e
sopurdey <a_purdey@hotmail.com>
2022-01-26 17:09:40 +0100

    Ajout ControllerAbsence & ControllerJourFerie

---
+++ projet_final/src/main/java/fr/diginamic/projet_final/repository/iCrudJourFerie.java
@@ -0,0 +1,12 @@
+package fr.diginamic.projet_final.repository;
+
+
+import org.springframework.data.repository.CrudRepository;
+import fr.diginamic.projet_final.model.JourFerie;
+
+
+public interface iCrudJourFerie extends CrudRepository<JourFerie, Integer> {
+
+	
+	
+}
