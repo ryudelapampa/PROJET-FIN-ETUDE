aee3f8960730097060d52bba787f74a00fd3bebf
sopurdey <a_purdey@hotmail.com>
2022-01-26 17:21:27 +0100

    maj ControllerGeneralError

--- projet_final/src/main/java/fr/diginamic/projet_final/controllerrest/ControllerGeneralError.java
+++ projet_final/src/main/java/fr/diginamic/projet_final/controllerrest/ControllerGeneralError.java
@@ -5,7 +5,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import fr.diginamic.projet_final.exception.AbsenceNotFoundException;
 import fr.diginamic.projet_final.exception.CollaborateurNotFoundException;
+import fr.diginamic.projet_final.exception.JourFerieNotFoundException;
 
 @RestControllerAdvice
 public class ControllerGeneralError {
@@ -17,6 +19,16 @@ public class ControllerGeneralError {
 	@ExceptionHandler(value = {Exception.class})
 	public ResponseEntity<String> onError(Exception ex){
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Other Error :" +ex.getMessage());
+	}
+	
+	@ExceptionHandler(value = {AbsenceNotFoundException.class})
+	public ResponseEntity<String> onErrorAbsence(AbsenceNotFoundException ex){
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body("AbsenceError :" +ex.getMessage());
+	}
+	
+	@ExceptionHandler(value = {JourFerieNotFoundException.class})
+	public ResponseEntity<String> onErrorJourFerie(JourFerieNotFoundException ex){
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body("AbsenceError :" +ex.getMessage());
 	}
 	
 	@ExceptionHandler(value = {CollaborateurNotFoundException.class})
