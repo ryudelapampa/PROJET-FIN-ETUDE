aa3735880b9ab45c441f2547ae80c0101dd97eda
Garcia <jimmygarcian@outlook.fr>
2022-01-26 14:51:37 +0100

    EXCEPTION + CONTROLLER COLLAB

---
+++ projet_final/src/main/java/fr/diginamic/projet_final/controllerrest/ControllerCollaborateur.java
@@ -0,0 +1,14 @@
+package fr.diginamic.projet_final.controllerrest;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping("api/collaborateur")
+public class ControllerCollaborateur {
+
+	public ControllerCollaborateur() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
